ae17535e10e5a6a0eb57d1f9bebac802470af080
notaddtry <comatose.9999@mail.ru>
2022-04-04 21:41:00 +0300

    upd for no env check

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app'
 console.log(process.env)
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDING_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCUbmREgTcDPA9NJYXLCrBHhpDOKXewloA',
+  authDomain: 'authcontact-c6b2e.firebaseapp.com',
+  projectId: 'authcontact-c6b2e',
+  storageBucket: 'authcontact-c6b2e.appspot.com',
+  messagingSenderId: '1054085479697',
+  appId: '1:1054085479697:web:7ec454a45817664c6120b0',
 }
 
 const app = initializeApp(firebaseConfig)
